1fc197c3308620ee121fab703a8424d7939734d3
jtorr <jtorr@JUAN-PC>
2025-10-05 22:24:06 -0300

    cambios en main

--- WindowsFormsApp1/Main.Designer.cs
+++ WindowsFormsApp1/Main.Designer.cs
@@ -28,140 +28,144 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnEstadoCuentaCorriente = new System.Windows.Forms.Button();
-            this.btnEstadoGuia = new System.Windows.Forms.Button();
-            this.btnResultadoCostosVentas = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
-            this.button7 = new System.Windows.Forms.Button();
-            this.button8 = new System.Windows.Forms.Button();
             this.grpConsultas = new System.Windows.Forms.GroupBox();
+            this.btnEstadoCuentaCorriente = new System.Windows.Forms.Button();
+            this.btnResultadoCostosVentas = new System.Windows.Forms.Button();
+            this.btnEstadoGuia = new System.Windows.Forms.Button();
             this.grpOperacion = new System.Windows.Forms.GroupBox();
+            this.btnForm4 = new System.Windows.Forms.Button();
+            this.btnEmitirFactura = new System.Windows.Forms.Button();
+            this.btnImposicionCallCenter = new System.Windows.Forms.Button();
+            this.btnRendicionyAsignacionPrimeraMilla = new System.Windows.Forms.Button();
+            this.btnImposicionyAdmisionDeEncomiendasEnCD = new System.Windows.Forms.Button();
+            this.grpConsultas.SuspendLayout();
+            this.grpOperacion.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // btnEstadoCuentaCorriente
-            // 
-            this.btnEstadoCuentaCorriente.Location = new System.Drawing.Point(217, 224);
-            this.btnEstadoCuentaCorriente.Name = "btnEstadoCuentaCorriente";
-            this.btnEstadoCuentaCorriente.Size = new System.Drawing.Size(150, 64);
-            this.btnEstadoCuentaCorriente.TabIndex = 0;
-            this.btnEstadoCuentaCorriente.Text = "Estado de Cuenta Corriente";
-            this.btnEstadoCuentaCorriente.UseVisualStyleBackColor = true;
-            // 
-            // btnEstadoGuia
-            // 
-            this.btnEstadoGuia.Location = new System.Drawing.Point(40, 224);
-            this.btnEstadoGuia.Name = "btnEstadoGuia";
-            this.btnEstadoGuia.Size = new System.Drawing.Size(150, 64);
-            this.btnEstadoGuia.TabIndex = 1;
-            this.btnEstadoGuia.Text = "Estado de Guía";
-            this.btnEstadoGuia.UseVisualStyleBackColor = true;
-            // 
-            // btnResultadoCostosVentas
-            // 
-            this.btnResultadoCostosVentas.Location = new System.Drawing.Point(396, 224);
-            this.btnResultadoCostosVentas.Name = "btnResultadoCostosVentas";
-            this.btnResultadoCostosVentas.Size = new System.Drawing.Size(150, 64);
-            this.btnResultadoCostosVentas.TabIndex = 2;
-            this.btnResultadoCostosVentas.Text = "Resultado Costos-Ventas";
-            this.btnResultadoCostosVentas.UseVisualStyleBackColor = true;
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(109, 76);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(121, 23);
-            this.button4.TabIndex = 3;
-            this.button4.Text = "Emitir Factura";
-            this.button4.UseVisualStyleBackColor = true;
-            // 
-            // button5
-            // 
-            this.button5.Location = new System.Drawing.Point(26, 120);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(123, 23);
-            this.button5.TabIndex = 4;
-            this.button5.Text = "Imposición Call Center";
-            this.button5.UseVisualStyleBackColor = true;
-            // 
-            // button6
-            // 
-            this.button6.Location = new System.Drawing.Point(155, 120);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(254, 23);
-            this.button6.TabIndex = 5;
-            this.button6.Text = "Imposición y Admisión de Encomiendas en CD";
-            this.button6.UseVisualStyleBackColor = true;
-            // 
-            // button7
-            // 
-            this.button7.Location = new System.Drawing.Point(26, 165);
-            this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(191, 23);
-            this.button7.TabIndex = 6;
-            this.button7.Text = "Rendición y Asignación Primera Milla";
-            this.button7.UseVisualStyleBackColor = true;
-            // 
-            // button8
-            // 
-            this.button8.Location = new System.Drawing.Point(334, 165);
-            this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(75, 23);
-            this.button8.TabIndex = 7;
-            this.button8.Text = "button8";
-            this.button8.UseVisualStyleBackColor = true;
             // 
             // grpConsultas
             // 
-            this.grpConsultas.Location = new System.Drawing.Point(26, 203);
+            this.grpConsultas.Controls.Add(this.btnEstadoCuentaCorriente);
+            this.grpConsultas.Controls.Add(this.btnResultadoCostosVentas);
+            this.grpConsultas.Controls.Add(this.btnEstadoGuia);
+            this.grpConsultas.Location = new System.Drawing.Point(16, 287);
             this.grpConsultas.Name = "grpConsultas";
             this.grpConsultas.Size = new System.Drawing.Size(536, 100);
-            this.grpConsultas.TabIndex = 8;
+            this.grpConsultas.TabIndex = 12;
             this.grpConsultas.TabStop = false;
             this.grpConsultas.Text = "Consultas";
             // 
+            // btnEstadoCuentaCorriente
+            // 
+            this.btnEstadoCuentaCorriente.Location = new System.Drawing.Point(189, 24);
+            this.btnEstadoCuentaCorriente.Name = "btnEstadoCuentaCorriente";
+            this.btnEstadoCuentaCorriente.Size = new System.Drawing.Size(150, 64);
+            this.btnEstadoCuentaCorriente.TabIndex = 3;
+            this.btnEstadoCuentaCorriente.Text = "Estado de Cuenta Corriente";
+            this.btnEstadoCuentaCorriente.UseVisualStyleBackColor = true;
+            // 
+            // btnResultadoCostosVentas
+            // 
+            this.btnResultadoCostosVentas.Location = new System.Drawing.Point(368, 24);
+            this.btnResultadoCostosVentas.Name = "btnResultadoCostosVentas";
+            this.btnResultadoCostosVentas.Size = new System.Drawing.Size(150, 64);
+            this.btnResultadoCostosVentas.TabIndex = 5;
+            this.btnResultadoCostosVentas.Text = "Resultado Costos-Ventas";
+            this.btnResultadoCostosVentas.UseVisualStyleBackColor = true;
+            // 
+            // btnEstadoGuia
+            // 
+            this.btnEstadoGuia.Location = new System.Drawing.Point(12, 24);
+            this.btnEstadoGuia.Name = "btnEstadoGuia";
+            this.btnEstadoGuia.Size = new System.Drawing.Size(150, 64);
+            this.btnEstadoGuia.TabIndex = 4;
+            this.btnEstadoGuia.Text = "Estado de Guía";
+            this.btnEstadoGuia.UseVisualStyleBackColor = true;
+            // 
             // grpOperacion
             // 
-            this.grpOperacion.Location = new System.Drawing.Point(374, 14);
+            this.grpOperacion.Controls.Add(this.btnForm4);
+            this.grpOperacion.Controls.Add(this.btnEmitirFactura);
+            this.grpOperacion.Controls.Add(this.btnImposicionCallCenter);
+            this.grpOperacion.Controls.Add(this.btnRendicionyAsignacionPrimeraMilla);
+            this.grpOperacion.Controls.Add(this.btnImposicionyAdmisionDeEncomiendasEnCD);
+            this.grpOperacion.Location = new System.Drawing.Point(105, 12);
             this.grpOperacion.Name = "grpOperacion";
-            this.grpOperacion.Size = new System.Drawing.Size(301, 100);
-            this.grpOperacion.TabIndex = 9;
+            this.grpOperacion.Size = new System.Drawing.Size(353, 259);
+            this.grpOperacion.TabIndex = 13;
             this.grpOperacion.TabStop = false;
             this.grpOperacion.Text = "Operación";
+            // 
+            // btnForm4
+            // 
+            this.btnForm4.Location = new System.Drawing.Point(189, 98);
+            this.btnForm4.Name = "btnForm4";
+            this.btnForm4.Size = new System.Drawing.Size(150, 64);
+            this.btnForm4.TabIndex = 7;
+            this.btnForm4.Text = "Form4";
+            this.btnForm4.UseVisualStyleBackColor = true;
+            // 
+            // btnEmitirFactura
+            // 
+            this.btnEmitirFactura.Location = new System.Drawing.Point(95, 179);
+            this.btnEmitirFactura.Name = "btnEmitirFactura";
+            this.btnEmitirFactura.Size = new System.Drawing.Size(150, 64);
+            this.btnEmitirFactura.TabIndex = 3;
+            this.btnEmitirFactura.Text = "Emitir Factura";
+            this.btnEmitirFactura.UseVisualStyleBackColor = true;
+            // 
+            // btnImposicionCallCenter
+            // 
+            this.btnImposicionCallCenter.Location = new System.Drawing.Point(12, 19);
+            this.btnImposicionCallCenter.Name = "btnImposicionCallCenter";
+            this.btnImposicionCallCenter.Size = new System.Drawing.Size(150, 64);
+            this.btnImposicionCallCenter.TabIndex = 4;
+            this.btnImposicionCallCenter.Text = "Imposición Call Center";
+            this.btnImposicionCallCenter.UseVisualStyleBackColor = true;
+            // 
+            // btnRendicionyAsignacionPrimeraMilla
+            // 
+            this.btnRendicionyAsignacionPrimeraMilla.Location = new System.Drawing.Point(12, 98);
+            this.btnRendicionyAsignacionPrimeraMilla.Name = "btnRendicionyAsignacionPrimeraMilla";
+            this.btnRendicionyAsignacionPrimeraMilla.Size = new System.Drawing.Size(150, 64);
+            this.btnRendicionyAsignacionPrimeraMilla.TabIndex = 6;
+            this.btnRendicionyAsignacionPrimeraMilla.Text = "Rendición y Asignación Primera Milla";
+            this.btnRendicionyAsignacionPrimeraMilla.UseVisualStyleBackColor = true;
+            // 
+            // btnImposicionyAdmisionDeEncomiendasEnCD
+            // 
+            this.btnImposicionyAdmisionDeEncomiendasEnCD.Location = new System.Drawing.Point(189, 19);
+            this.btnImposicionyAdmisionDeEncomiendasEnCD.Name = "btnImposicionyAdmisionDeEncomiendasEnCD";
+            this.btnImposicionyAdmisionDeEncomiendasEnCD.Size = new System.Drawing.Size(150, 64);
+            this.btnImposicionyAdmisionDeEncomiendasEnCD.TabIndex = 5;
+            this.btnImposicionyAdmisionDeEncomiendasEnCD.Text = "Imposición y Admisión de Encomiendas en CD";
+            this.btnImposicionyAdmisionDeEncomiendasEnCD.UseVisualStyleBackColor = true;
             // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(824, 432);
-            this.Controls.Add(this.btnEstadoCuentaCorriente);
-            this.Controls.Add(this.button8);
-            this.Controls.Add(this.button7);
-            this.Controls.Add(this.button6);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.btnResultadoCostosVentas);
-            this.Controls.Add(this.btnEstadoGuia);
+            this.ClientSize = new System.Drawing.Size(571, 401);
             this.Controls.Add(this.grpConsultas);
             this.Controls.Add(this.grpOperacion);
             this.Name = "Main";
             this.Text = "Menú Principal";
+            this.grpConsultas.ResumeLayout(false);
+            this.grpOperacion.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button btnEstadoCuentaCorriente;
-        private System.Windows.Forms.Button btnEstadoGuia;
-        private System.Windows.Forms.Button btnResultadoCostosVentas;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Button button6;
-        private System.Windows.Forms.Button button7;
-        private System.Windows.Forms.Button button8;
         private System.Windows.Forms.GroupBox grpConsultas;
+        private System.Windows.Forms.Button btnEstadoCuentaCorriente;
+        private System.Windows.Forms.Button btnResultadoCostosVentas;
+        private System.Windows.Forms.Button btnEstadoGuia;
         private System.Windows.Forms.GroupBox grpOperacion;
+        private System.Windows.Forms.Button btnForm4;
+        private System.Windows.Forms.Button btnEmitirFactura;
+        private System.Windows.Forms.Button btnImposicionCallCenter;
+        private System.Windows.Forms.Button btnRendicionyAsignacionPrimeraMilla;
+        private System.Windows.Forms.Button btnImposicionyAdmisionDeEncomiendasEnCD;
     }
 }
