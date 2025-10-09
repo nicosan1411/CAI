7f1a3f6941aa806494c79401f9cc88270680298a
MALENA AZUL TORRES ASTIGUETA <54TO43665223@campus.economicas.uba.ar>
2025-10-09 19:37:48 -0300

    Agregue pantalla entrega a cliente en CD, falta agregarle el list view

---
+++ WindowsFormsApp1/EntregaEnCD/EntregaEnCDForm.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace WindowsFormsApp1.EntregaEnCD
+{
+    partial class EntregaEnCDForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnvolvermenu = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(105, 74);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(581, 263);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Comprobante de Entrega";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "DNI";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(82, 34);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(146, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(223, 197);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(139, 34);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Imprimir";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // btnvolvermenu
+            // 
+            this.btnvolvermenu.Location = new System.Drawing.Point(493, 406);
+            this.btnvolvermenu.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnvolvermenu.Name = "btnvolvermenu";
+            this.btnvolvermenu.Size = new System.Drawing.Size(193, 33);
+            this.btnvolvermenu.TabIndex = 20;
+            this.btnvolvermenu.Text = "Volver al Menú Principal";
+            this.btnvolvermenu.UseVisualStyleBackColor = true;
+            // 
+            // EntregaEnCDForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(735, 450);
+            this.Controls.Add(this.btnvolvermenu);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "EntregaEnCDForm";
+            this.Text = "EntregaEnCDForm";
+            this.Load += new System.EventHandler(this.EntregaEnCDForm_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnvolvermenu;
+    }
+}
--- WindowsFormsApp1/FormInicio.Designer.cs
+++ WindowsFormsApp1/FormInicio.Designer.cs
@@ -38,6 +38,7 @@
             this.btnImposicionCallCenter = new System.Windows.Forms.Button();
             this.btnRendicionPrimeraMilla = new System.Windows.Forms.Button();
             this.btnAdmisionCD = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.grpConsultas.SuspendLayout();
             this.grpOperacion.SuspendLayout();
             this.SuspendLayout();
@@ -47,18 +48,21 @@
             this.grpConsultas.Controls.Add(this.btnCuentaCorriente);
             this.grpConsultas.Controls.Add(this.btnResultadosCostosVentas);
             this.grpConsultas.Controls.Add(this.btnEstadoGuia);
-            this.grpConsultas.Location = new System.Drawing.Point(16, 287);
+            this.grpConsultas.Location = new System.Drawing.Point(21, 353);
+            this.grpConsultas.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.grpConsultas.Name = "grpConsultas";
-            this.grpConsultas.Size = new System.Drawing.Size(536, 100);
+            this.grpConsultas.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.grpConsultas.Size = new System.Drawing.Size(715, 123);
             this.grpConsultas.TabIndex = 12;
             this.grpConsultas.TabStop = false;
             this.grpConsultas.Text = "Consultas";
             // 
             // btnCuentaCorriente
             // 
-            this.btnCuentaCorriente.Location = new System.Drawing.Point(189, 24);
+            this.btnCuentaCorriente.Location = new System.Drawing.Point(252, 30);
+            this.btnCuentaCorriente.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnCuentaCorriente.Name = "btnCuentaCorriente";
-            this.btnCuentaCorriente.Size = new System.Drawing.Size(150, 64);
+            this.btnCuentaCorriente.Size = new System.Drawing.Size(200, 79);
             this.btnCuentaCorriente.TabIndex = 6;
             this.btnCuentaCorriente.Text = "Estado de Cuenta Corriente";
             this.btnCuentaCorriente.UseVisualStyleBackColor = true;
@@ -66,9 +70,10 @@
             // 
             // btnResultadosCostosVentas
             // 
-            this.btnResultadosCostosVentas.Location = new System.Drawing.Point(368, 24);
+            this.btnResultadosCostosVentas.Location = new System.Drawing.Point(491, 30);
+            this.btnResultadosCostosVentas.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnResultadosCostosVentas.Name = "btnResultadosCostosVentas";
-            this.btnResultadosCostosVentas.Size = new System.Drawing.Size(150, 64);
+            this.btnResultadosCostosVentas.Size = new System.Drawing.Size(200, 79);
             this.btnResultadosCostosVentas.TabIndex = 7;
             this.btnResultadosCostosVentas.Text = "Resultados Costos-Ventas";
             this.btnResultadosCostosVentas.UseVisualStyleBackColor = true;
@@ -76,9 +81,10 @@
             // 
             // btnEstadoGuia
             // 
-            this.btnEstadoGuia.Location = new System.Drawing.Point(12, 24);
+            this.btnEstadoGuia.Location = new System.Drawing.Point(16, 30);
+            this.btnEstadoGuia.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnEstadoGuia.Name = "btnEstadoGuia";
-            this.btnEstadoGuia.Size = new System.Drawing.Size(150, 64);
+            this.btnEstadoGuia.Size = new System.Drawing.Size(200, 79);
             this.btnEstadoGuia.TabIndex = 5;
             this.btnEstadoGuia.Text = "Estado de Guía";
             this.btnEstadoGuia.UseVisualStyleBackColor = true;
@@ -86,33 +92,38 @@
             // 
             // grpOperacion
             // 
+            this.grpOperacion.Controls.Add(this.button1);
             this.grpOperacion.Controls.Add(this.btnForm4);
             this.grpOperacion.Controls.Add(this.btnEmitirFactura);
             this.grpOperacion.Controls.Add(this.btnImposicionCallCenter);
             this.grpOperacion.Controls.Add(this.btnRendicionPrimeraMilla);
             this.grpOperacion.Controls.Add(this.btnAdmisionCD);
-            this.grpOperacion.Location = new System.Drawing.Point(105, 12);
+            this.grpOperacion.Location = new System.Drawing.Point(140, 15);
+            this.grpOperacion.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.grpOperacion.Name = "grpOperacion";
-            this.grpOperacion.Size = new System.Drawing.Size(353, 259);
+            this.grpOperacion.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.grpOperacion.Size = new System.Drawing.Size(471, 319);
             this.grpOperacion.TabIndex = 13;
             this.grpOperacion.TabStop = false;
             this.grpOperacion.Text = "Operación";
             // 
             // btnForm4
             // 
-            this.btnForm4.Location = new System.Drawing.Point(189, 98);
+            this.btnForm4.Location = new System.Drawing.Point(252, 121);
+            this.btnForm4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnForm4.Name = "btnForm4";
-            this.btnForm4.Size = new System.Drawing.Size(150, 64);
+            this.btnForm4.Size = new System.Drawing.Size(200, 79);
             this.btnForm4.TabIndex = 3;
-            this.btnForm4.Text = "Form4";
+            this.btnForm4.Text = "Entregas y Recepciones Micros";
             this.btnForm4.UseVisualStyleBackColor = true;
             this.btnForm4.Click += new System.EventHandler(this.btnForm4_Click);
             // 
             // btnEmitirFactura
             // 
-            this.btnEmitirFactura.Location = new System.Drawing.Point(95, 179);
+            this.btnEmitirFactura.Location = new System.Drawing.Point(252, 218);
+            this.btnEmitirFactura.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnEmitirFactura.Name = "btnEmitirFactura";
-            this.btnEmitirFactura.Size = new System.Drawing.Size(150, 64);
+            this.btnEmitirFactura.Size = new System.Drawing.Size(200, 79);
             this.btnEmitirFactura.TabIndex = 4;
             this.btnEmitirFactura.Text = "Emitir Factura";
             this.btnEmitirFactura.UseVisualStyleBackColor = true;
@@ -120,9 +131,10 @@
             // 
             // btnImposicionCallCenter
             // 
-            this.btnImposicionCallCenter.Location = new System.Drawing.Point(12, 19);
+            this.btnImposicionCallCenter.Location = new System.Drawing.Point(16, 23);
+            this.btnImposicionCallCenter.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnImposicionCallCenter.Name = "btnImposicionCallCenter";
-            this.btnImposicionCallCenter.Size = new System.Drawing.Size(150, 64);
+            this.btnImposicionCallCenter.Size = new System.Drawing.Size(200, 79);
             this.btnImposicionCallCenter.TabIndex = 0;
             this.btnImposicionCallCenter.Text = "Imposición Call Center";
             this.btnImposicionCallCenter.UseVisualStyleBackColor = true;
@@ -130,31 +142,44 @@
             // 
             // btnRendicionPrimeraMilla
             // 
-            this.btnRendicionPrimeraMilla.Location = new System.Drawing.Point(12, 98);
+            this.btnRendicionPrimeraMilla.Location = new System.Drawing.Point(16, 121);
+            this.btnRendicionPrimeraMilla.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnRendicionPrimeraMilla.Name = "btnRendicionPrimeraMilla";
-            this.btnRendicionPrimeraMilla.Size = new System.Drawing.Size(150, 64);
+            this.btnRendicionPrimeraMilla.Size = new System.Drawing.Size(200, 79);
             this.btnRendicionPrimeraMilla.TabIndex = 2;
-            this.btnRendicionPrimeraMilla.Text = "Rendir y Asignar Primera Milla";
+            this.btnRendicionPrimeraMilla.Text = "Rendir y Asignar Fletes";
             this.btnRendicionPrimeraMilla.UseVisualStyleBackColor = true;
             this.btnRendicionPrimeraMilla.Click += new System.EventHandler(this.btnRendicionPrimeraMilla_Click);
             // 
             // btnAdmisionCD
             // 
-            this.btnAdmisionCD.Location = new System.Drawing.Point(189, 19);
+            this.btnAdmisionCD.Location = new System.Drawing.Point(252, 23);
+            this.btnAdmisionCD.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnAdmisionCD.Name = "btnAdmisionCD";
-            this.btnAdmisionCD.Size = new System.Drawing.Size(150, 64);
+            this.btnAdmisionCD.Size = new System.Drawing.Size(200, 79);
             this.btnAdmisionCD.TabIndex = 1;
             this.btnAdmisionCD.Text = "Imposición y Admisión de Encomiendas en CD";
             this.btnAdmisionCD.UseVisualStyleBackColor = true;
             this.btnAdmisionCD.Click += new System.EventHandler(this.btnAdmisionCD_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 218);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 79);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Entregas en CD";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // FormInicio
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(571, 401);
+            this.ClientSize = new System.Drawing.Size(761, 494);
             this.Controls.Add(this.grpConsultas);
             this.Controls.Add(this.grpOperacion);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "FormInicio";
             this.Text = "Menú Principal";
             this.grpConsultas.ResumeLayout(false);
@@ -175,5 +200,6 @@
         private System.Windows.Forms.Button btnImposicionCallCenter;
         private System.Windows.Forms.Button btnRendicionPrimeraMilla;
         private System.Windows.Forms.Button btnAdmisionCD;
+        private System.Windows.Forms.Button button1;
     }
 }
