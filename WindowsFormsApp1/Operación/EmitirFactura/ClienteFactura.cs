67af262da0c6e22d30a172699ca09f02e39f8a01
nicosan1411 <nicosan1411@gmail.com>
2025-10-24 20:15:18 -0300

    Cambios en emitir factura

---
+++ WindowsFormsApp1/Operación/EmitirFactura/ClienteFactura.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1.Operación.EmitirFactura
+{
+    public class ClienteFactura
+    {
+        
+                public string Cuit { get; set; }
+                public string RazonSocial { get; set; }
+
+       public string Cuit_RazonSocial
+        {
+            get { return $"({Cuit}) {RazonSocial}"; }
+        }
+    }
+        }
+
+    
+
+
+    
---
+++ WindowsFormsApp1/Operación/EmitirFactura/ListadoFacturacion.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1.Operación.EmitirFactura
+{
+    public class ListadoFacturacion
+    {
+
+        public string Cliente { get; set; }
+        public string NroGuia { get; set; }
+        public string Concepto { get; set; }
+        public decimal Monto { get; set; }
+
+
+        public ListadoFacturacion(string cliente, string nroGuia, string concepto, decimal monto)
+        {
+            Cliente = cliente;
+            NroGuia = nroGuia;
+            Concepto = concepto;
+            Monto = monto;
+                 }
+    }
+}
+
