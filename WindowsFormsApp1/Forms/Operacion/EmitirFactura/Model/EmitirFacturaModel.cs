902da7cfdb590bff3dbc5bcd04ef20cb4a9afb23
nicosan1411 <nicosan1411@gmail.com>
2025-11-02 15:40:25 -0300

    cambio descripción concepto factura, sólo cambio de texto, no cambio lógica código

--- WindowsFormsApp1/Forms/Operacion/EmitirFactura/Model/EmitirFacturaModel.cs
+++ WindowsFormsApp1/Forms/Operacion/EmitirFactura/Model/EmitirFacturaModel.cs
@@ -25,11 +25,11 @@ namespace CAI_Proyecto.Forms.Operacion.EmitirFactura.Model
         {
             ListadoFacturacion = new List<ListadoFacturacion>
             {
-                new ListadoFacturacion("30-50109269-6  Unilever de Argentina S.A.", "2001", "Flete y entrega a domicilio", 8200.00m),
-                new ListadoFacturacion("30-50361405-3  Arcor S.A.I.C.", "2002", "Servicio de transporte regional", 6500.00m),
-                new ListadoFacturacion("30-70752101-7  Molinos Río de la Plata S.A.", "2003", "Entrega en CD destino", 9700.00m),
-                new ListadoFacturacion("30-50033372-9  Coca-Cola FEMSA S.A.", "2004", "Entrega a cliente final", 11300.00m),
-                new ListadoFacturacion("30-56712390-1  Procter & Gamble S.R.L.", "2005", "Transporte nacional", 9800.00m)
+                new ListadoFacturacion("30-50109269-6  Unilever de Argentina S.A.", "2001", "Desde CD a domicilio", 8200.00m),
+                new ListadoFacturacion("30-50361405-3  Arcor S.A.I.C.", "2002", "Desde CD a CD", 6500.00m),
+                new ListadoFacturacion("30-70752101-7  Molinos Río de la Plata S.A.", "2003", "Desde agencia a domicilio", 9700.00m),
+                new ListadoFacturacion("30-50033372-9  Coca-Cola FEMSA S.A.", "2004", "Desde agencia a domicilio", 11300.00m),
+                new ListadoFacturacion("30-56712390-1  Procter & Gamble S.R.L.", "2005", "Desde CD a agencia", 9800.00m)
             };
         }
     }
