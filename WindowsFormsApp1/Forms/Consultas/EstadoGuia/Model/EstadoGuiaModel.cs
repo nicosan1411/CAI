7859d1e7a301031470a8d6d77752b9843a511c06
MALENA AZUL TORRES ASTIGUETA <54TO43665223@campus.economicas.uba.ar>
2025-11-06 17:50:05 -0300

    genere datos para almacen, micros, fleteros y empresasfletes

--- WindowsFormsApp1/Forms/Consultas/EstadoGuia/Model/EstadoGuiaModel.cs
+++ WindowsFormsApp1/Forms/Consultas/EstadoGuia/Model/EstadoGuiaModel.cs
@@ -7,15 +7,7 @@ namespace CAI_Proyecto.Forms.Consultas.EstadoGuia.Model
 {
     public class EstadoGuiaModel
     {
-        // Datos de prueba internos
-        /*private readonly List<Guia> _demo = new List<Guia>
-        {
-            new Guia("1001", "M", "Centro de Distribución Rosario",  "Envío a domicilio", "32455678", "18-07-2020", "Admitida en CD destino"),
-            new Guia("1002", "L", "Centro de Distribución CABA",     "Envío a domicilio", "33122345", "25-06-2021", "Entregada a cliente"),
-            new Guia("1003", "S", "Centro de Distribución Córdoba",  "Envío a agencia",   "29877654", "05-09-2024", "En tránsito"),
-            new Guia("1004", "M", "Centro de Distribución Tucumán",  "Envío a domicilio", "31233987", "08-10-2025", "Impuesta")
-        }; */
-
+     
         /*
          * Devuelve una lista con los números de todas las guías disponibles en el almacén.
          * Se usa para llenar el ComboBox de selección de guía en el formulario.
